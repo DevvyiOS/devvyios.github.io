80c689429e3db5c810b5169284246e07cb1311a3
DevvyiOS <68819524+DevvyiOS@users.noreply.github.com>
2020-07-31 10:07:32 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+	<html>
+		<body>
+			<head>
+				<title>About Devvy</title>
+				<meta name="viewport" content="width=device-width, in initial-scale=1">
+				<link rel="stylesheet" href="main.css">
+				<div class="navbar">
+					<a href="index.php">Home</a>
+					<a class="active" href="about.php">About</a>
+					<a href="https://twitter.com/devvylul" target="_blank">Twitter</a>
+					<a href="https://github.com/DevvyiOS" target="_blank">GitHub</a>
+				</div>
+			</head>
+			<center>
+				<div class="textcard">
+					<h1>About Devvy</h1>
+					<p>I am a 17 year old developer learning to be a security researcher.</p>
+				</div>
+			</center>
+		</body>
+	</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+	<html>
+		<body>
+			<head>
+				<title>Devvy Stuff</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<link rel="stylesheet" href="main.css">
+				<div class="navbar">
+					<a class="active" href="index.php">Home</a>
+					<a href="about.php">About</a>
+					<a href="https://twitter.com/devvylul" target="_blank">Twitter</a>
+					<a href="https://github.com/DevvyiOS" target="_blank">GitHub</a>
+				</div>		
+			</head>
+			<center>
+				<div class="card">
+					<img src="idev.jpg">
+					<h1>DevvyiOS Repo</h1>
+					<p>Click "Copy" to copy my repo.</p>
+					<a href="index.php">Copy</a>
+				</div>
+				
+				<div class="card">
+					<img src="idev.jpg">
+					<h1>DevvyiOS Repo</h1>
+					<p>Click "Copy" to copy my repo.</p>
+					<a href="index.php">Copy</a>
+				</div>
+				
+				<div class="card">
+					<img src="idev.jpg">
+					<h1>DevvyiOS Repo</h1>
+					<p>Click "Copy" to copy my repo.</p>
+					<a href="index.php">Copy</a>
+				</div>
+			<center>
+		</body>
+	</html>
